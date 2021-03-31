3ff1efd7a161bf8ae7a6bb225dda108b69b128e8
Reductions <slavi.boyanov@gmail.com>
2021-03-31 12:22:53 +0300

    Fix analyzer warnings

--- test/project_agent_test.dart
+++ test/project_agent_test.dart
@@ -1,9 +1,9 @@
+import 'package:fs_test_agent/dart_project_agent.dart';
 import 'package:test/test.dart';
-import 'package:command_line_agent/command_line_agent.dart';
 
 void main() {
   test("Create agent", () {
-    final agent = ProjectAgent("test_project");
-    print(agent.workingDirectory);
+    final agent = DartProjectAgent("test_project");
+    print(agent.workingDirectory); //ignore: avoid_print
   });
 }
